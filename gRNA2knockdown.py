2fb749e945d9897795997d247b1aa403846ffb72
Alec Taylor <alectaylor76@gmail.com>
2024-05-07 11:59:07 -0700

    step size

--- gRNA2knockdown.py
+++ gRNA2knockdown.py
@@ -546,3 +546,17 @@ if __name__ == "__main__":
                     this_embed_loss=this_embed_loss,  
                     batchsize=batch_size_parameter,
                     step_size_val=this_step_size_val*10.0,max_iters=5e4)
+
+    import seaborn as sns;
+    all_mismatches = []
+    for ind in range(0,len(this_corpus_vec)):
+        z_ind = this_y_out.eval(feed_dict={this_u:[this_corpus_vec[ind]]}, session=sess)
+        this_seq_out = g2k.vecback2seq(np.dot(np.linalg.inv(Rand_Transform),z_ind.T))
+        print(g2k.vecback2seq(np.dot(np.linalg.inv(Rand_Transform),z_ind.T)))
+        this_seq_out = ''.join(this_seq_out)
+        all_mismatches.append(g2k.num_mismatch(this_seq_out,this_corpus[ind]));
+    hist_data = sns.displot(all_mismatches)
+    mismatch_process = np.array(all_mismatches);
+    mismatch_process[mismatch_process<=2.0] = 1.0;
+    mismatch_process[mismatch_process>2.0] = 0.0;
+    np.sum(mismatch_process)/(len(mismatch_process)*1.0)    
