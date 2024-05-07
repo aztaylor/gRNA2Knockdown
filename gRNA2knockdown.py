31660da7bbbaae9b431fcd1ebb566fd98993ec68
Alec Taylor <alectaylor76@gmail.com>
2024-05-07 11:41:52 -0700

    changed batch size

--- gRNA2knockdown.py
+++ gRNA2knockdown.py
@@ -487,7 +487,7 @@ if __name__ == "__main__":
     label_dim = 1
     embedding_dim = 28
     intermediate_dim = 50
-    batch_size_parameter=300 #4000 for howard's e. coli dataset (from Enoch's code)
+    batch_size_parameter=27 #4000 for howard's e. coli dataset (from Enoch's code)
     debug_splash = 0
     this_step_size_val = 0.25
     this_corpus,this_labels = make_labeled_corpus(allseqs, data, stride_parameter)
