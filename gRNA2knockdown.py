a78d2b33348f1f8241ac7583ead5e9d47aee57c6
Alec Taylor <alectaylor76@gmail.com>
2024-05-29 17:37:56 -0700

    Commented out the all embedding traingg data plot because it caused an error

--- gRNA2knockdown.py
+++ gRNA2knockdown.py
@@ -398,7 +398,7 @@ def train_net(sess, u_all_training:np.array, u_feed:tf.Variable,
         ax.plot(x,training_error_history_nocovar,label='train. err.')
         ax.plot(x,validation_error_history_nocovar,label='valid. err.')
         ax.plot(x,test_error_history_nocovar,label='test err.')
-        ax.plot(x,u_all_training,label='Reconstruction Loss')
+        #ax.plot(x,u_all_training,label='Reconstruction Loss')
         ax.legend()
         ax.set_xlabel('Iterations')
         ax.set_ylabel('Error')
