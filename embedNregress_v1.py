1bc902cca384f1a0c8c208caf5ce71d3e1a87b15
Alec Taylor <alectaylor76@gmail.com>
2024-07-25 15:58:31 -0700

    little changes to fix a bug

--- embedNregress_v1.py
+++ embedNregress_v1.py
@@ -516,7 +516,7 @@ if __name__ == "__main__":
     this_induced_fl_data = data1[FL_key]
     
     this_time = time0[FL_key][0:360]
-    foldchangedata = np.zeros((8*12,360))
+    foldchangedata = np.zeros((8,12,360))
     for row in range(0,8):
         for col in range(0,12):
             print(this_induced_fl_data.shape)
