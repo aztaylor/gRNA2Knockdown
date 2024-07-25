8f861b651101be429b336ef822603da1100de04a
Alec Taylor <alectaylor76@gmail.com>
2024-07-25 15:56:39 -0700

    better bug fixing message

--- embedNregress_v1.py
+++ embedNregress_v1.py
@@ -515,7 +515,7 @@ if __name__ == "__main__":
     this_induced_od_data = data1[OD_key]
     this_induced_fl_data = data1[FL_key]
     
-    this_time = time0[FL_key]
+    this_time = time0[FL_key][0:360]
     foldchangedata = data0[FL_key]-data0[FL_key] # makes a zeros matrix with the right dimensions for storing fold change data 
     for row in range(0,8):
         for col in range(0,12):
