b64652f4a7fe2f180fb344e375f5acd47053c504
Alec Taylor <alectaylor76@gmail.com>
2024-07-25 14:04:57 -0700

    Changed some of the strings to f-strings

--- embedNregress_v1.py
+++ embedNregress_v1.py
@@ -577,16 +577,18 @@ if __name__ == "__main__":
 
         if True:
             # Train the network
-            train_figure_name = "Figures/Training{0}_{1}_{2}_{3}.png".format(
-                embedding_dim,intermediate_dim,stride_parameter,
-                    n_pre_post_layers)
+            train_figure_name = f"Figures/Training{embedding_dim}_{intermediate_dim}_{stride_parameter}_{n_pre_post_layers}.png"
             train_net(sess, this_corpus_vec,this_u,HybridLoss,
-                    this_optim,this_vae_loss=this_vae_loss,
+                    this_optim,
+                    this_vae_loss=this_vae_loss,
                     this_embed_loss=this_embed_loss,  
                     batchsize=batch_size_parameter,
-                    step_size_val=this_step_size_val,max_iters=max_iters,
+                    step_size_val=this_step_size_val,
+                    max_iters=max_iters,
                     save_fig= train_figure_name)
     # This is likely redudent code that can be removed.
+    # I think we just need to reconsider the loss function at this point.
+
     feedforwardList = [embedding_dim]+[feedforwardDim]*feedforwardDepth+\
         [outpuDim]
     with tf.device('/gpu:0'):
