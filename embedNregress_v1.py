57a60fcf4cad8db87cabc32f2d740ac6a19cc696
Enoch Yeung <enochyeung@Mac-Studio-2.local>
2024-09-03 12:40:57 -0700

    embedding dim at 18

--- embedNregress_v1.py
+++ embedNregress_v1.py
@@ -520,7 +520,7 @@ if __name__ == "__main__":
     induced_fl_data = data1[FL_key]
 
     StartHorizon =0
-    EndHorizon= 180
+    EndHorizon= 60
     time_vec = time0[FL_key][StartHorizon:EndHorizon]
     foldchangedata = np.zeros((8,12,EndHorizon-StartHorizon))
     for row in range(0,8):
