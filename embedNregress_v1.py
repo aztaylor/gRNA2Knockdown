3eca7a70c1041a2149baab7d844615a8ef9571ca
Alec Taylor <alectaylor76@gmail.com>
2024-09-24 15:36:35 -0700

    normalized embedding norm

--- embedNregress_v1.py
+++ embedNregress_v1.py
@@ -561,7 +561,7 @@ if __name__ == "__main__":
     #training parameters
     debug_splash = 0
     this_step_size_val = 0.05
-    this_max_iters = 3e4
+    this_max_iters = 1e6
     this_corpus,this_labels = make_labeled_corpus(allseqs, data, stride_parameter)
     print(this_corpus)
     # Define the corpus for the model.
